986b533b402896cade6bee80680dbad5e1f53975
Riccardo Brero <riki.brero@gmail.com>
2021-04-28 23:34:42 +0200

    Upgrade

--- fluido/lib/fluido.dart
+++ fluido/lib/fluido.dart
@@ -13,14 +13,14 @@
 /// To globally customize breakpoints use [FluidoSettings].
 library fluido;
 
-export 'package:fluido/fluido_settings.dart';
-export 'package:fluido/screen_switch_value.dart';
-export 'package:fluido/layout_switch_widget.dart';
+export 'package:fluido/layout/layout_orientation.dart';
+export 'package:fluido/layout/layout_switch_widget.dart';
+export 'package:fluido/layout/layout_width.dart';
 
-export 'package:fluido/screen_width.dart';
-export 'package:fluido/layout_width.dart';
-export 'package:fluido/screen_orientation.dart';
-export 'package:fluido/layout_orientation.dart';
-export 'package:fluido/screen_width_wrap.dart';
+export 'package:fluido/screen/screen_orientation.dart';
+export 'package:fluido/screen/screen_switch_value.dart';
+export 'package:fluido/screen/screen_width.dart';
+export 'package:fluido/screen/screen_width_wrap.dart';
 
 export 'package:fluido/fluido_context.dart';
+export 'package:fluido/fluido_settings.dart';
